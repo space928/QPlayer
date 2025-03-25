e57cd008c4dfa5dc78acaf37c25052ebe3a02ea2
Thomas Mathieson <thomas@mathieson.dev>
2025-03-25 05:10:54 +0100

    Many more improvements to the docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import starlightAutoSidebar from 'starlight-auto-sidebar'
+import starlightAutoSidebar from 'starlight-auto-sidebar';
+import starlightGiscus from 'starlight-giscus';
+import starlightKbd from 'starlight-kbd'
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +12,24 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'QPlayer Documentation',
-			plugins: [starlightAutoSidebar()],
+			plugins: [
+				starlightAutoSidebar(),
+				starlightGiscus({
+					repo: 'space928/QPlayer',
+					repoId: 'R_kgDOLSwoIA',
+					category: 'Comments',
+					categoryId: 'DIC_kwDOLSwoIM4CobZs',
+					mapping: '<title>',
+					reactions: true,
+					lazy: true
+				}),
+				starlightKbd({
+					types: [
+						{ id: 'mac', label: 'macOS' },
+						{ id: 'windows', label: 'Windows', default: true },
+					],
+				}),
+			],
 			logo: {
 				src: './src/assets/Splash.png',
 				replacesTitle: true,
